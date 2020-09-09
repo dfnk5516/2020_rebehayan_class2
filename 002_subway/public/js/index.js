75cec2a093b6fb166d725424f07a1281092e8c31
dfnk5516 <rayshoo@naver.com>
2020-09-10 04:51:53 +0900

    commit

--- 002_subway/public/js/index.js
+++ 002_subway/public/js/index.js
@@ -1,7 +1,12 @@
+import { quickMenuItems, subMenuItems } from './menuItems.js';
+
 const openSideButton = document.querySelector('.btn_sidemenu');
 const closeSideButton = document.querySelector('.side_close');
 const dim = document.querySelector('.dim');
 const sideBar = document.querySelector('.sidebar_wrapper');
+const quickMenus = document.querySelector('.quick_menus > ul');
+const subMenus = document.querySelector('.sub_menus > ul');
+const imagePrefix = 'images/';
 
 openSideButton.addEventListener('click', () => {
   sideBar.classList.remove('close');
@@ -15,3 +20,45 @@ closeSideButton.addEventListener('click', () => {
   dim.classList.remove('open');
   dim.classList.add('close');
 });
+
+quickMenuItems.forEach(n=>{
+  let tempLi = document.createElement('li');
+  let tempA = document.createElement('a');
+  tempA.href = n.href;
+  tempA.innerText = n.text;
+  tempA.style.background = `url(${imagePrefix}${n.background}) no-repeat center top / 2.25rem auto`;
+  tempLi.appendChild(tempA);
+  quickMenus.appendChild(tempLi);
+})
+
+subMenuItems.forEach(n=>{
+  let tempLi = document.createElement('li');
+  let tempSpan = document.createElement('span');
+  let tempUl = document.createElement('ul');
+  
+  n.children.forEach(n=>{
+    let tempA = document.createElement('a'); 
+    tempA.innerText = n.text;
+    tempA.href = n.href;
+    tempUl.classList.add('children');
+    tempUl.innerHTML += `<li>${tempA.outerHTML}</li>`;
+  })
+
+  tempSpan.innerText = n.text;
+  tempSpan.style.background = `url(${imagePrefix}${n.background}) no-repeat .75rem center / 1.3rem auto, #fff`;
+  tempLi.addEventListener('click', e=>{
+    if(e.target.tagName !== 'SPAN') return;
+
+    tempLi.classList.contains('active')
+      ? (()=>tempLi.classList.remove('active'))()
+      : (()=>tempLi.classList.add('active'))()
+  })
+
+  tempLi.appendChild(tempSpan);
+  tempLi.appendChild(tempUl);
+  subMenus.appendChild(tempLi);
+})
+
+
+
+
---
+++ 002_subway/public/js/menuItems.js
@@ -0,0 +1,150 @@
+export const quickMenuItems = [
+  {
+    text : 'FAST-SUB',
+    background : 'ico_quick_fast.png',
+    href : '#'
+  },
+  {
+    text : 'HOME-SUB',
+    background : 'ico_quick_home.png',
+    href : '#'
+  },
+  {
+    text : '단체주문',
+    background : 'ico_quick_group.png',
+    href : '#'
+  },
+  {
+    text : '써브카드',
+    background : 'ico_quick_subcard.png',
+    href : '#'
+  },
+  {
+    text : '쿠폰',
+    background : 'ico_quick_coupon.png',
+    href : '#'
+  },
+  {
+    text : 'e-Gift',
+    background : 'ico_quick_gift.png',
+    href : '#'
+  },
+];
+export const subMenuItems = [
+  {
+    text : '메뉴소개',
+    background : 'ico_menu.png',
+    children : [
+      {
+        text : '샌드위치',
+        href : '#', 
+      },
+      {
+        text : '랩',
+        href : '#', 
+      },
+      {
+        text : '찹샐러드',
+        href : '#', 
+      },
+      {
+        text : '스마일 썹',
+        href : '#', 
+      },
+      {
+        text : '단체메뉴',
+        href : '#', 
+      },
+    ]
+  },
+  {
+    text : '이용방법',
+    background : 'ico_howto.png',
+    children : [
+      {
+        text : '써브웨이 이용방법',
+        href : '#',
+      },
+      {
+        text : '단체메뉴 이용방법',
+        href : '#',
+      },
+      {
+        text : '신선한 재료 소개',
+        href : '#',
+      },
+    ]
+  },
+  {
+    text : '새소식',
+    background : 'ico_news.png',
+    children : [
+      {
+        text : '이벤트ㆍ프로모션',
+        href : '#',
+      },
+      {
+        text : '뉴스ㆍ공지사항',
+        href : '#',
+      },
+      {
+        text : '광고영상',
+        href : '#',
+      },
+    ]
+  },
+  {
+    text : '써브웨이',
+    background : 'ico_subway.png',
+    children : [
+      {
+        text : '써브웨이 역사',
+        href : '#',
+      },
+      {
+        text : '써브웨이 약속',
+        href : '#',
+      },
+      {
+        text : '매장찾기',
+        href : '#',
+      },
+    ]
+  },
+  {
+    text : '가맹점',
+    background : 'ico_store.png',
+    children : [
+      {
+        text : '써브웨이 프랜차이즈',
+        href : '#',
+      },
+      {
+        text : '가맹관련 FAQ',
+        href : '#',
+      },
+      {
+        text : '지사안내',
+        href : '#',
+      },
+      {
+        text : '사업설명회(대표번호 02-797-5036)',
+        href : '#',
+      },
+    ]
+  },
+  {
+    text : '고객센터',
+    background : 'ico_customer.png',
+    children : [
+      {
+        text : 'FAQ',
+        href : '#',
+      },
+      {
+        text : '1:1문의',
+        href : '#',
+      },
+    ]
+  },
+];
